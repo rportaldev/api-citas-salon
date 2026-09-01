4ebde81d529180a44b24fc63f590f29d047d9387
Dev Zero <litkaponii@gmail.com>
2026-08-31 20:15:03 -0500

    feat: creación inicial del proyecto API Citas Salon

---
+++ src/main/java/com/rportaldev/apicitassalon/ApiCitasSalonApplication.java
@@ -0,0 +1,13 @@
+package com.rportaldev.apicitassalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCitasSalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCitasSalonApplication.class, args);
+	}
+
+}
